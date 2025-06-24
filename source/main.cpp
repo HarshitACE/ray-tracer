0ad38d919abf220304ca559bdc3bd04dd2a2a504
HarshitACE <gharshit17@gmail.com>
2025-06-24 22:20:00 +0100

    feat: add antialiasing and camera class.

--- source/main.cpp
+++ source/main.cpp
@@ -5,6 +5,7 @@
 #include "objects/sphere.hpp"
 #include "rtweekend.hpp"
 #include "libs/color.hpp"
+#include "libs/camera.hpp"
 #include "objects/hittable_list.hpp"
 
 double hit_sphere(const point3& centre, double radius, const ray& r) {
@@ -40,7 +41,7 @@ auto main() -> int {
   const auto aspect_ratio = 16.0 / 9.0;
   const int image_width = 400;
   const int image_height = static_cast<int>(image_width / aspect_ratio);
-  const double blue_val = 0.25;
+  const int samples_per_pixel = 100;
 
   // World
   hittable_list world;
@@ -48,14 +49,7 @@ auto main() -> int {
   world.add(std::make_shared<sphere>(point3(0,-100.5,-1), 100));
 
   // Camera
-  auto viewport_height = 2.0;
-  auto viewport_width = aspect_ratio * viewport_height;
-  auto focal_length = 1.0;  // Distance between viewport plane and camera's orign
-
-  point3 origin{0, 0, 0};
-  vec3 horizontal{viewport_width, 0, 0};
-  vec3 vertical{0, viewport_height, 0};
-  auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3{0, 0, focal_length};
+  camera cam;
 
   // Render
   std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
@@ -63,11 +57,14 @@ auto main() -> int {
   for (int j = image_height - 1; j >= 0; --j) {
     std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
     for (int i = 0; i < image_width; ++i) {
-      auto u = static_cast<double>(i) / (image_width - 1);
-      auto v = static_cast<double>(j) / (image_height - 1);
-      ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
-      color pixel_color = ray_color(r, world);
-      write_color(std::cout, pixel_color);
+        color pixel_color(0, 0, 0);
+        for (int s = 0; s < samples_per_pixel; ++s) {
+            auto u = (i + random_double()) / (image_width - 1);
+            auto v = (j + random_double()) / (image_height - 1);
+            ray r = cam.get_ray(u, v);
+            pixel_color += ray_color(r, world);
+        }
+        write_color(std::cout, pixel_color, samples_per_pixel);
     }
   }
 
--- source/rtweekend.hpp
+++ source/rtweekend.hpp
@@ -1,6 +1,7 @@
 #ifndef RTWEEKEND_H
 #define RTWEEKEND_H
 #include <cmath>
+#include <random>
 #include <limits>
 
 // Constants
@@ -10,6 +11,25 @@ const double pi = 3.1415926535897932385;
 // Utility functions
 inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
+}
+
+inline auto random_double() -> double {
+    // Returns a random real in [0, 1)
+    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    static std::mt19937 generator;
+    return distribution(generator);
+}
+
+inline auto random_double(double min, double max) -> double {
+    static std::uniform_real_distribution<double> distribution(min, max);
+    static std::mt19937 generator;
+    return distribution(generator);
+}
+
+inline double clamp(double x, double min, double max) {
+    if (x < min) return min;
+    if (x > max) return max;
+    return x;
 }
 
 // Common Headers

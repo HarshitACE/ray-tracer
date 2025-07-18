348fcd4a4e687d56857a5dfdd210e4fa16e7f3f2
HarshitACE <gharshit17@gmail.com>
2025-07-18 22:42:31 +0100

    feat: metal reflection fuzziness

--- source/objects/metal.hpp
+++ source/objects/metal.hpp
@@ -4,14 +4,16 @@
 #include "material.hpp"
 
 class metal : public material {
-    public:
-        color albedo;
+ public:
+  color albedo;
+  double fuzz;
 
-        metal(const color& a) : albedo(a) {}
-        virtual auto scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const -> bool override {
-            vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
-            scattered = ray(rec.p, reflected);
-            attenuation = albedo;
-            return (dot(scattered.direction(), rec.normal) > 0);
-        }
+  metal(const color& a, double f) : albedo(a), fuzz(f < 1 ? f : 1) {}
+  virtual auto scatter(const ray& r_in, const hit_record& rec, color& attenuation,
+                       ray& scattered) const -> bool override {
+    vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
+    scattered = ray(rec.p, reflected + fuzz * random_in_unit_sphere());
+    attenuation = albedo;
+    return (dot(scattered.direction(), rec.normal) > 0);
+  }
 };

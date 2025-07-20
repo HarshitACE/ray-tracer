34d762653f8d4a59495c447b97ebbd98351107da
HarshitACE <gharshit17@gmail.com>
2025-07-21 00:46:17 +0100

    feat: add a bubble in the world

--- source/main.cpp
+++ source/main.cpp
@@ -69,6 +69,7 @@ auto main() -> int {
   world.add(std::make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
   world.add(std::make_shared<sphere>(point3(0, 0, -1), 0.5, material_center));
   world.add(std::make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
+  world.add(std::make_shared<sphere>(point3(-1.0, 0.0, -1.0), -0.4, material_left));
   world.add(std::make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
 
   // Camera

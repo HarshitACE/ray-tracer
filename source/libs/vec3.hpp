177da0f21ade4765a4ab19abcbed9e45ca555cde
HarshitACE <gharshit17@gmail.com>
2025-07-23 22:59:21 +0100

    feat: depth of field and positionable camera

--- source/libs/vec3.hpp
+++ source/libs/vec3.hpp
@@ -114,11 +114,11 @@ inline auto reflect(const vec3& v, const vec3& n) {
   return v - 2 * dot(v, n) * n;
 }
 
-inline auto refract(const vec3& uv, const vec3&n, double etai_over_etat) {
-    auto cos_theta = fmin(dot(-uv, n), 1.0);
-    vec3 r_out_perp = etai_over_etat * (uv + cos_theta*n);
-    vec3 r_out_para = -sqrt(fabs(1.0 - r_out_perp.length_squared())) * n;
-    return r_out_perp + r_out_para;
+inline auto refract(const vec3& uv, const vec3& n, double etai_over_etat) {
+  auto cos_theta = fmin(dot(-uv, n), 1.0);
+  vec3 r_out_perp = etai_over_etat * (uv + cos_theta * n);
+  vec3 r_out_para = -sqrt(fabs(1.0 - r_out_perp.length_squared())) * n;
+  return r_out_perp + r_out_para;
 }
 
 inline auto random_unit_vector() -> vec3 {
@@ -131,4 +131,14 @@ inline auto random_in_hemisphere(const vec3& normal) -> vec3 {
     return in_unit_sphere;
   }
   return -in_unit_sphere;
+}
+
+inline auto random_in_unit_disk() -> vec3 {
+  while (true) {
+    auto p = vec3(random_double(-1, 1), random_double(-1, 1), 0);
+    if (p.length_squared() >= 1) {
+      continue;
+    }
+    return p;
+  }
 }
--- source/rtweekend.hpp
+++ source/rtweekend.hpp
@@ -9,7 +9,7 @@ const double infinity = std::numeric_limits<double>::infinity();
 const double pi = 3.1415926535897932385;
 
 // Utility functions
-inline double degrees_to_radians(double degrees) {
+inline auto degrees_to_radians(double degrees) -> double {
     return degrees * pi / 180.0;
 }
 
